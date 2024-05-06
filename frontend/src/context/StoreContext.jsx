9cf15c299f9a87a381e56142a8b2f12cdb7354eb
kathanpatel29 <69028584+kathanpatel29@users.noreply.github.com>
2024-05-06 13:33:56 -0400

    Food Display and Food Item Added

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext,useState } from "react"
+import PropTypes from 'prop-types'
+import { food_list } from "../assets/assets"
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+    const[cartItems,setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+    }
+    const contextValue = {
+        food_list
+    }
+    return(
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+StoreContextProvider.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default StoreContextProvider
