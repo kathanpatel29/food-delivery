384e4851cead3e33353b11b6c8e16f242f9b9304
kathanpatel29 <69028584+kathanpatel29@users.noreply.github.com>
2024-05-06 15:35:12 -0400

    Built the pages responsive

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,24 +1,25 @@
-import {React,useState} from 'react'
-import PropTypes from 'prop-types'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
 
-  const [itemCount,setItemCount] = useState(0)
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
-        <img className='food-item-image' src={image} alt=''/>
-      {!itemCount
-      ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
-      :<div className='food-item-counter'>
-        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-        <p>{itemCount}</p>
-        <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
-      </div>
-      }
+        <img className='food-item-image' src={image} alt='' />
+        {!cartItems[id] ? (
+          <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+        ) : (
+          <div className='food-item-counter'>
+            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
@@ -29,15 +30,15 @@ const FoodItem = ({ id, name, price, description, image }) => {
         <p className="food-item-price">${price}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 FoodItem.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired
-}
+  image: PropTypes.string.isRequired,
+};
 
-export default FoodItem
+export default FoodItem;
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext,useState } from "react"
+import {React, createContext,useState,useEffect } from "react"
 import PropTypes from 'prop-types'
 import { food_list } from "../assets/assets"
 
@@ -11,9 +11,26 @@ const StoreContextProvider = (props) => {
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
         }
+        else {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
     }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
+    
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart 
     }
     return(
         <StoreContext.Provider value={contextValue}>
