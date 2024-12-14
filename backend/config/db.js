19555af07db91fb12b8050b6c5a36819d79ced80
kathanpatel29 <69028584+kathanpatel29@users.noreply.github.com>
2024-12-13 20:52:50 -0500

    vercel.json added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kathanpatel29:kathan%40290700007@cluster0.k1u26gn.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DB Connected"));
 }
