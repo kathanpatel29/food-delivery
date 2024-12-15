640c99fb33fd133a332cafa6bbcfb4cd4cd24cee
kathanpatel29 <69028584+kathanpatel29@users.noreply.github.com>
2024-12-15 13:53:03 -0500

    header img fixed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import header_img from '../../assets/header_img.jpg'
 
 const Header = () => {
   return (
-    <div className='header'style={{ backgroundImage: `url(${headerImg})` }}>
+    <div className='header'style={{ backgroundImage: `url(${header_img})` }}>
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our main goal is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
