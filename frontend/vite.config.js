bb44584ab0c75ca7f31d7d1f66f308bd4b0deb63
kathanpatel29 <69028584+kathanpatel29@users.noreply.github.com>
2024-12-15 13:12:31 -0500

    default

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // You can specify a public directory if it's different
-  publicDir: 'public',
-});
+  plugins: [react()],
+})
